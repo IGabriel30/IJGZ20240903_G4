f83504b99c2ece12cebc2641bcb8642a321e33ce
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-03 20:35:37 -0600

    AccountEndpoint

---
+++ IJGZ20240903/Endpoints/AccountEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using IJGZ20240903.Auth;
+using System.Runtime.CompilerServices;
+
+namespace IJGZ20240903.Endpoints
+{
+    public static class AccountEndpoint
+    {
+        public static void AddAccountEndpoints(this WebApplication app) 
+        {
+            app.MapPost("/account/login", (string login, string password, IJwtAuthenticationService authService) =>
+            {
+                //comprueba si las credenciales de inicio de sesión son válidas
+                if(login == "admin" && password == "12345")
+                {
+                    //si son válidas, autentica al usuairo
+                    //utilizando el servico de autenticacion JWT y obtiene un token
+                    var token = authService.Authenticate(login);
+
+                    //devuelve una respuesta HTTP OK (200) con el token JWT como resultado
+                    return Results.Ok(token);
+                }
+                else
+                {
+                    //si las credenciales no son válidas, devuele una repsuesta HTTP Unauthorized(401).
+                    return Results.Unauthorized();
+                }
+            });
+
+        }
+    }
+}
